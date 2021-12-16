4c2e41dbf1ca8277cd7f8dd6f27b6178cbf674ff
NewbieDevPro <89529297+NewbieDevPro@users.noreply.github.com>
2021-12-16 16:32:33 +0000

    new

--- js/Dropdown.js
+++ js/Dropdown.js
@@ -14,3 +14,16 @@ window.onclick = function(event) {
         }
     }
 }
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+    }
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false)
